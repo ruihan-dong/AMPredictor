2687701ffd7f60ee1275dcaefac8920f35e896f5
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-05-27 10:04:54 +0200

    refactor: add arparser

--- input/fasta2txt.py
+++ input/fasta2txt.py
@@ -4,19 +4,29 @@
 
 from Bio import SeqIO
 import pandas as pd
-name = []
-sequence = []
-label=[]
-i=0
+from argparse import ArgumentParser
 
-file_name = 'input'
-seq = file_name + '.fasta'
-output = file_name + '.txt'
-for seq_record in SeqIO.parse(seq, "fasta"):
-    name.append(str(seq_record.id))
-    sequence.append(str(seq_record.seq))
-    i=1+i
-    label.append(int(i))
 
-df = pd.DataFrame(data={'Name': name, 'SequenceID': sequence,'Label':label})
-df.to_csv(output, sep=' ', index=False, header=False)
+def convert(filename, output_name):
+    name = []
+    sequence = []
+    label = []
+    i = 0
+
+    for seq_record in SeqIO.parse(filename, "fasta"):
+        name.append(str(seq_record.id))
+        sequence.append(str(seq_record.seq))
+        i = 1 + i
+        label.append(int(i))
+
+    df = pd.DataFrame(data={'Name': name, 'SequenceID': sequence, 'Label': label})
+    df.to_csv(output_name, sep=' ', index=False, header=False)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("input")
+    parser.add_argument("output")
+    args = parser.parse_args()
+    convert(args.input, args.output)
+    print("Done")
